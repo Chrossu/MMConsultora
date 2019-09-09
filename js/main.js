26ef14954317077876e845ac0df2d9a24056dc9b
Chrossu <Chrossu@users.noreply.github.com>
2019-09-08 22:14:09 -0300

    Added video and carousel

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 // Smooth Scrolling
-$('a, .btn').on('click', function(event) {
-    if (this.hash !== '') {
-      event.preventDefault();
-  
-      const hash = this.hash;
-  
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top - 40
-        },
-        1200
-      );
-    }
+$('a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 40
+      },
+      1200
+    );
+  }
+});
+
+// Slick carousel
+$(document).ready(function () {
+  $('.autoplay').slick({
+    dots: false,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
   });
+});
